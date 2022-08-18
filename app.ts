b5a0992e573391590805af537d3e5e1943a6564d
Gustavo <gusvicenzi12@gmail.com>
2022-08-17 21:46:48 -0300

    Mudança app.ts

--- app.ts
+++ app.ts
@@ -1,4 +1,3 @@
-import * as express from "express"
 import { myDataSource } from "./app-data-source"
 
 // establish database connection
@@ -10,13 +9,3 @@ myDataSource
     .catch((err) => {
         console.error("Error during Data Source initialization:", err)
     })
-
-// create and setup express app
-const app = express()
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
-
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-    console.log(`Executando em http://localhost:${PORT}`)
-})
